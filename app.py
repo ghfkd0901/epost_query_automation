262aef587b1d2365398b59a790a2f9ac9ca05895
ghfkd0901 <ghfkd0901@gmail.com>
2025-12-15 09:49:54 +0900

    wow3

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.chrome.service import Service # V24: Service 모듈 import 제거
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
